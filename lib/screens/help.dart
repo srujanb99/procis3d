ae2a496684bf0f138d46cc9f5e4637f25ab86275
srujanb99 <srujanb7@gmail.com>
2021-06-28 02:45:39 +0530

    Added functionality and customization

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:procis3d/components/scaffold_theme.dart';
+import 'package:procis3d/constants.dart';
 
 class Help extends StatelessWidget {
   @override
@@ -7,8 +8,14 @@ class Help extends StatelessWidget {
     return ScaffoldTheme(
       title: 'Help & Documentation',
       body: Center(
-        child: Container(
-          child: Text('Help & Documentation'),
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Container(
+            child: Text(
+              'SelectionPage:\n1. Select the subject associated with the 3D figure required.\n2. Select the appropriate topic.\n3. Choose either theory or formula based on the use case.\n4. Select the theory or formula and hit next to get the 3D representation.\n\nOutput Page:\n1. Drag the 3D object around to rotate it.\n2. Click on \'save image\' to have the snapshot of the object saved locally.\n\nAny suggestions or feedback will be appreciated and can be submitted through the About page.',
+              style: kFormTitle,
+            ),
+          ),
         ),
       ),
     );
--- lib/screens/param_output.dart
+++ lib/screens/param_output.dart
@@ -1,7 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:procis3d/components/scaffold_theme.dart';
 import 'package:procis3d/constants.dart';
 import 'package:procis3d/objects_3d.dart';
+import 'package:screenshot/screenshot.dart';
 
 class ParamsOutput extends StatelessWidget {
   @override
@@ -24,6 +27,7 @@ class Output extends StatefulWidget {
 }
 
 class _OutputState extends State<Output> {
+  ScreenshotController screenshotController = ScreenshotController();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,12 +41,15 @@ class _OutputState extends State<Output> {
         ),
         Expanded(
           flex: 4,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.white,
+          child: Screenshot(
+            controller: screenshotController,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: Objects3D(),
             ),
-            child: Objects3D(),
           ),
         ),
         SizedBox(
@@ -54,13 +61,38 @@ class _OutputState extends State<Output> {
           ),
         ),
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            screenshotController
+                .capture(delay: Duration(milliseconds: 10))
+                .then((capturedImage) async {
+              showCapturedWidget(context, capturedImage!);
+            }).catchError((onError) {
+              print(onError);
+            });
+          },
           style: ElevatedButton.styleFrom(
             primary: Colors.deepPurpleAccent,
           ),
           child: Text('Save Image'),
         ),
       ],
+    );
+  }
+
+  Future<dynamic> showCapturedWidget(
+      BuildContext context, Uint8List capturedImage) {
+    return showDialog(
+      useSafeArea: false,
+      context: context,
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: Text("Captured widget screenshot"),
+        ),
+        body: Center(
+            child: capturedImage != null
+                ? Image.memory(capturedImage)
+                : Container()),
+      ),
     );
   }
 }
